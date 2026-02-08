b14e5b5b4f588a42f32d57d1441843657df664e5
HubkoNazar <hubko.nazar@gmail.com>
2026-02-08 15:15:39 +0200

    scroll button and new pages

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById("themeBtn").onclick = function () {
     document.body.classList.toggle("gray");
 };
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.classList.remove("show");
+    }
+});
+
+scrollBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
